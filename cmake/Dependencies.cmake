839a7c28ab17c61fe5a408279f515acb49c494a9
lcskrishna <lollachaitanya@gmail.com>
2018-09-19 13:38:00 -0700

    enabled debug symbols for hipcc

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -545,6 +545,10 @@ if(NOT BUILD_ATEN_MOBILE)
     list(APPEND HIP_HIPCC_FLAGS -Wno-shift-count-negative)
     list(APPEND HIP_HIPCC_FLAGS -Wno-shift-count-overflow)
     list(APPEND HIP_HIPCC_FLAGS -Wno-unused-command-line-argument)
+    if(CMAKE_BUILD_TYPE MATCHES Debug)
+       list(APPEND HIP_HCC_FLAGS -g)
+       list(APPEND HIP_HCC_FLAGS -O0)
+    endif(CMAKE_BUILD_TYPE MATCHES Debug)
 
     set(Caffe2_HIP_INCLUDES
       ${hip_INCLUDE_DIRS} ${hcc_INCLUDE_DIRS} ${hsa_INCLUDE_DIRS} ${rocrand_INCLUDE_DIRS} ${hiprand_INCLUDE_DIRS} ${rocblas_INCLUDE_DIRS} ${miopen_INCLUDE_DIRS} ${thrust_INCLUDE_DIRS} $<INSTALL_INTERFACE:include> ${Caffe2_HIP_INCLUDES})

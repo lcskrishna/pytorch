577eff3d8c2afb571664a2a363e2b84067fc4bb7
lcskrishna <lollachaitanya@gmail.com>
2019-05-22 14:54:42 -0700

    added miopen_rnn_flatten_weights in config files

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -139,6 +139,11 @@ DONT_ENFORCE_SAME_TENSOR_IMPL_OR_STORAGE = {
     # These functions are expected to change impl or storage of input tensors
     'set_', '_cudnn_rnn_flatten_weight',
 }
+
+DONT_ENFORCE_SAME_TENSOR_IMPL_OR_STORAGE = {
+    # These functions are expected to change impl or storage of input tensors
+    '_th_set_', 'miopen_rnn_flatten_weight',
+}
 # END CHECKS FOR [ Invariant: TensorImpl and Storage Pointer Equality ]
 
 METHOD_DECLARATION = CodeTemplate("""\
